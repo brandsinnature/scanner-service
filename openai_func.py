6f365d8caa01bac36fd2cd7a73c7f66d438e6164
dg_20104 <amitgupta@iign.ac.in>
2025-02-28 22:01:28 +0530

    changing model

--- openai_func.py
+++ openai_func.py
@@ -7,7 +7,7 @@ def setup_openai_client(api_key):
 def detect_object_openai(client, image_data):
     try:
         response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4-turbo",
             messages=[
                 {"role": "system", "content": "You need to detect the object in the image and provide me just a jsonl output which would have the following fields. 'detections' which is a list of objects detected. Each object should have 'id', 'product_name', 'product_company', 'material', 'confidence', fields."},
                 {"role": "user", "content": [
